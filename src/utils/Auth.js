9c2545cde13af3b41bf04bb9a47959a81afb1fda
Konstantin Suvorov <huxt-huxt@yandex.ru>
2022-11-21 19:02:37 +0300

    4th_stage

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.huxt-huxt.nomoredomains.icu';
+export const BASE_URL = 'https://api.huxt-huxt.nomoredomains.icu';
 
 export const register = (name, email, password) => fetch(`${BASE_URL}/signup`, {
   method: 'POST',
--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.huxt-huxt.nomoredomains.icu';
+export const BASE_URL = 'https://api.huxt-huxt.nomoredomains.icu';
 
 export const getUserData = () => fetch(`${BASE_URL}/users/me`, {
   method: 'GET',
